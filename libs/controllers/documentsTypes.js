ffbad4970f40c596d943eb7143ac13f9013483d6
richCodeAma <richard.appiah@amalitech.com>
2022-10-26 22:16:48 +0000

    uplaod document

--- libs/controllers/documentsTypes.js
+++ libs/controllers/documentsTypes.js
@@ -33,4 +33,21 @@ const getDocumentTypes = async (req, res, next) => {
   }
 };
 
-module.exports = { getDocumentTypes };
+const uploadDocumentTypes = async (req, res, next) => {
+  try {
+    let URL = `${process.env.SERVICEURL}/service/docxon/`;
+    const me = request(
+      { url: URL, method: 'POST', json: true, body: req.body },
+      function (err, results) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).json(JSON.parse(results.body));
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { getDocumentTypes, uploadDocumentTypes };
--- routes/documentsTypes.js
+++ routes/documentsTypes.js
@@ -3,5 +3,5 @@ const documentsTypesController = require('../libs/controllers/documentsTypes');
 const router = express.Router();
 
 router.get('/getdocumentsTypes', documentsTypesController.getDocumentTypes);
-
+router.post('/uploadDocument', documentsTypesController.uploadDocumentTypes);
 module.exports = router;
