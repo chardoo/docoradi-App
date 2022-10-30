ea19d84ec2f3f22668c222210037da449f3838ea
richCodeAma <richard.appiah@amalitech.com>
2022-10-30 20:07:53 +0000

    code refactoring

--- libs/controllers/documentsTypes.js
+++ libs/controllers/documentsTypes.js
@@ -3,31 +3,13 @@ const request = require('request');
 const getDocumentTypes = async (req, res, next) => {
   try {
     let URL = `${process.env.SERVICEURL}/service/documentTypes/`;
-    // console.log(URL);
-    // const response = await fetch(URL,{method: 'GET'});
-    // console.log(response);
     const me = request(URL, function (err, results) {
       if (err) {
         console.log(err);
       } else {
-        // console.log(results.body);
-
         res.status(200).json(JSON.parse(results.body));
-
-        //    const countDoctors =  JSON.stringify(results.body, (key, value) =>(typeof value ==='bigint' ? value.toString(): value))
-        //    const final =   JSON.parse(countDoctors, (key, value) => {
-        //        if (typeof value === "string" && value.startsWith('BIGINT::')) {
-        //          return BigInt(value.substr(8));
-        //        }
-        //        return value;
-        //      });
-
-        //   res.status(200).json(final);
       }
     });
-    //    console.log(me.response)
-    //     // const data = await response.json();
-    //     res.status(200).json(me);
   } catch (error) {
     next(error);
   }
@@ -42,7 +24,7 @@ const uploadDocumentTypes = async (req, res, next) => {
         if (err) {
           console.log(err);
         } else {
-          res.status(200).json(JSON.parse(results.body));
+          res.status(201).json(results.body.status);
         }
       }
     );
